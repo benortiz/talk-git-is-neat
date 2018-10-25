1c29837c963b9e5afa878a78be99e36598004a2f
Benjamin Ortiz <hello.ben.ortiz@gmail.com>
2018-10-24 19:43:19 -0700

    Squash and reset patch example

---
+++ 01b-squash.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+
+echo "You've thought about it, and it'd actually be best if new-file is one commit."
+echo "Use git rebase --interactive to squash everything into one commit."
+echo "Then, after more thought, you've decided that BBB should really be a separate commit."
+echo "Use git reset --patch to uncommit that one line and turn it into a new commit."
