8303c3f304893fe7da48d8d48c7685621f0e07d3
Benjamin Ortiz <hello.ben.ortiz@gmail.com>
2018-10-24 20:05:43 -0700

    Add further reading on git's internals

--- 00-reflog.sh
+++ 00-reflog.sh
@@ -10,3 +10,7 @@ echo "Resetting changes"
 git reset --hard master
 
 echo "Try using a combination of reflog and fsck --unreachable to find the changes"
+
+echo -e "\n\nIf you want to learn more about how git works internally, check these out:"
+echo "An in-depth disection: https://codewords.recurse.com/issues/two/git-from-the-inside-out"
+echo "A 30 minute talk: https://www.youtube.com/watch?v=Y2Msq90ZknI"
